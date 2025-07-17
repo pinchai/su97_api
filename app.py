9ba10b7cc795700c3ea5725f623c21e47dcd12d5
Pinchai <>
2025-07-17 11:33:56 +0700

    add to cart

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 
 app = Flask(__name__)
 
+
 # @app.get('/')
 # @app.get('/home')
 # def home():
@@ -22,6 +23,24 @@ def home():
     except Exception as e:
         error = e
     return render_template('home.html', products=products, error=error)
+
+
+@app.get('/detail/<int:pro_id>')
+def detail(pro_id):
+    product = None
+    error = ''
+    try:
+        r = requests.get(f"https://fakestoreapi.com/products/{pro_id}")
+        if r.status_code == 200:
+            product = r.json()
+    except Exception as e:
+        error = e
+    return render_template('detail.html', product=product, error=error)
+
+
+@app.get('/cart')
+def cart():
+    return render_template('cart.html')
 
 
 @app.get('/support')
